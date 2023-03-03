b082d84c60db0ecf80cfa150df1c58f1ad19e282
rian33svan <96176662+RianErick@users.noreply.github.com>
2023-03-03 13:31:25 -0300

    migration

--- src/main/resources/db/migration/V001__create_table_aluno.sql
+++ src/main/resources/db/migration/V001__create_table_aluno.sql
@@ -7,21 +7,19 @@ cpf varchar (11) not null,
 matricula varchar (15) not null,
 periodo varchar (50) not null,
 curso varchar (100) not null,
-
 primary key (id)
 
 );
 
 create table armario(
-
 id bigint not null auto_increment,
 aluno_id bigint references aluno_bolsista (id),
 cor varchar (40) not null,
 numero_chave varchar (8) not null,
 localizacao varchar (100) not null,
 status varchar (10) not null,
-
 primary key (id)
+
 );
 
 create table solicitacao_armario(
@@ -30,6 +28,6 @@ aluno_id bigint references aluno_bolsista(id),
 armario_id bigint references armario (id),
 data_sl_armario datetime not null,
 status_sl varchar (10) not null,
-
 primary key (id)
+
 );
