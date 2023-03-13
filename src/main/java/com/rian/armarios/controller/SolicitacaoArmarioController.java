2f19bb85362845403b4ef05c1956c8545c378d24
Edclydson <edclydson.sousa@gmail.com>
2023-03-13 17:42:15 -0300

    :books: docs(AlunoBolsistaController,ArmarioController,SolicitacaoArmarioController): Adicionando annotations do Swagger

--- src/main/java/com/rian/armarios/controller/SolicitacaoArmarioController.java
+++ src/main/java/com/rian/armarios/controller/SolicitacaoArmarioController.java
@@ -3,6 +3,8 @@ package com.rian.armarios.controller;
 import com.rian.armarios.model.SolicitacaoArmario;
 import com.rian.armarios.repository.SolicitacaoArmarioRepository;
 import com.rian.armarios.service.SolicitacaoService.SolicitacaoArmarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -29,18 +31,29 @@ public class SolicitacaoArmarioController {
     @PostMapping("/solicitacao")
     @ResponseStatus(HttpStatus.CREATED)
     @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "Solicitação realizada com sucesso"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Solicitação não encontrada"),
+            @ApiResponse(responseCode = "201", description = "Solicitação realizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Solicitação não encontrada"),
     })
+    @Operation(summary = "Solicita um armário")
     public SolicitacaoArmario solicitacao(@RequestBody @Valid SolicitacaoArmario solicitacao){
        return solicitacaoArmarioService.solicitarArmario(solicitacao);
 
     }
     @GetMapping("/consultar")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Solicitações listadas com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Solicitações não encontradas"),
+    })
+    @Operation(summary = "Lista todas as solicitações de armários")
     public List <SolicitacaoArmario> consulta (){
         return solicitacaoArmarioRepository.findAll();
     }
     @DeleteMapping("/deletar/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Solicitação deletada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Solicitação não encontrada"),
+    })
+    @Operation(summary = "Deleta uma solicitação de armário")
     public ResponseEntity <Void> deletar (@PathVariable Long id){
         if (!solicitacaoArmarioRepository.existsById(id)){
             return ResponseEntity.notFound().build();
