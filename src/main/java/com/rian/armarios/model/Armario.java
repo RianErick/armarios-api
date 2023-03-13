387147914d2ab5a63f8ac97abb708ae5c716f256
Edclydson <edclydson.sousa@gmail.com>
2023-03-13 17:47:13 -0300

    :books: docs(AlunoBolsista,Armario,SolicitacaoArmario): Mapeando entidades com Swagger

--- src/main/java/com/rian/armarios/model/Armario.java
+++ src/main/java/com/rian/armarios/model/Armario.java
@@ -2,6 +2,7 @@ package com.rian.armarios.model;
 
 
 import com.rian.armarios.model.Enums.StatusArmario;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -11,23 +12,29 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "armario")
+@Schema(description = "Armário", name = "Armario")
 public class Armario  {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
+    @Schema(description = "Id do armário", name = "id", required = true,type = "Long")
     private Long id;
 
     @NotBlank
+    @Schema(description = "Cor do armário", name = "cor", required = true,type = "String")
     private String cor;
 
     @NotBlank
     @Column(name = "numero_chave")
+    @Schema(description = "Número da chave do armário", name = "numeroChave", required = true,type = "String")
     private String numeroChave;
 
     @NotBlank
+    @Schema(description = "Localização do armário", name = "localizacao", required = true,type = "String")
     private String localizacao;
 
     @Enumerated(EnumType.STRING)
+    @Schema(description = "Status do armário", name = "status", required = true,type = "StatusArmario")
     private StatusArmario status = StatusArmario.DISPONIVEL;
 
 
