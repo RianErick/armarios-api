13ead68bf7a9a75fc7b59641c8f4238aa921240e
Edclydson <edclydson.sousa@gmail.com>
2023-03-13 15:01:24 -0300

    :recycle: refactor(SolicitacaoArmarioService): Removendo imports inutilizados e dependencia de ArmarioRepository

--- src/main/java/com/rian/armarios/service/SolicitacaoService/SolicitacaoArmarioService.java
+++ src/main/java/com/rian/armarios/service/SolicitacaoService/SolicitacaoArmarioService.java
@@ -1,21 +1,13 @@
 package com.rian.armarios.service.SolicitacaoService;
-import com.rian.armarios.model.Armario;
-import com.rian.armarios.model.Enums.StatusArmario;
-import com.rian.armarios.model.SolicitacaoArmario;
+
 import com.rian.armarios.model.Enums.StatusAluguel;
-import com.rian.armarios.repository.ArmarioRepository;
+import com.rian.armarios.model.SolicitacaoArmario;
 import com.rian.armarios.repository.SolicitacaoArmarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
-import org.springframework.format.datetime.DateFormatter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 
 
 @Service
@@ -23,9 +15,6 @@ public class SolicitacaoArmarioService {
 
     @Autowired
     private SolicitacaoArmarioRepository solicitacaoArmarioRepository;
-
-    @Autowired
-    private ArmarioRepository armarioRepository;
 
 
     @Transactional
